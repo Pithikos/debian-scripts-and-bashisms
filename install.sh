9f2dfeb65f33a47888fbe934e52ba2806ee5d2b1
Johan Hanssen Seferidis <manossef@gmail.com>
2015-12-23 14:10:04 +0000

    Refactored out

--- install.sh
+++ install.sh
@@ -36,42 +36,18 @@ sudo add-apt-repository ppa:videolan/stable-daily -y
 sudo apt-get update
 sudo apt-get install vlc -y
 
-
 # Graphics
 sudo apt-get install gimp inkscape -y
-
-
-# VirtualBox
-
 
 # Web & net
 sudo apt-get install wireshark -y
 sudo apt-get install chromium-browser -y
 sudo apt-get install deluge -y
 
-
-# Dev
-sudo apt-get install docker.io -y
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-sudo service docker restart
-
-sudo add-apt-repository ppa:webupd8team/atom -y
-sudo apt-get update
-sudo apt-get install atom -y
-
-sudo apt-get install geany -y
-cd /tmp
-git clone https://github.com/codebrainz/geany-themes.git
-mkdir -p ~/.config/geany/colorschemes/
-cp geany-themes/colorschemes/* ~/.config/geany/colorschemes/
-cd $OLD
-
-
-# Firefox extensions
-./install_firefox_addons.sh
-./install_unix_basic.sh
-
-
 # System tools
 sudo apt-get install gparted -y
+
+# Installation
+./install_unix_basic.sh
+./install_social.sh
+./install_firefox_addons.sh
