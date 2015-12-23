e8d04e5ff8c2a4db651ac5612ee4f0fc0da4c40e
Johan Hanssen Seferidis <manossef@gmail.com>
2015-12-23 13:24:53 +0000

    More code

--- Templates/bash.sh
+++ Templates/bash.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+
 # Looping through files
 for file in "`ls | grep .xpi`"; do
 	echo "$file"
 done
 
-# Check directory exists
+
+# Check directory doesn't exists
 if [ ! -d "$addon_id" ]; then
 	echo "Directory doesn't exist"
 fi
+
+
+# Check file exists with a function
+file_exists () {
+	if [ -f "$1" ]; then
+		echo "File exists"
+	else
+		echo "File doesn't exist"
+	fi
+}
