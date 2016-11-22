3213b53f34220c0016f7282f5871407bf230cd1c
Pithikos <manossef@hotmail.com>
2016-11-22 12:10:11 +0000

    Add bash script to install Gnome3 themes and icons

---
+++ gnome.sh
@@ -0,0 +1,10 @@
+# Adapta theme
+sudo add-apt-repository ppa:tista/adapta
+sudo apt-get update && sudo apt-get install adapta-gtk-theme adapta-backgrounds -y
+
+# Numix theme
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update && sudo apt-get install numix-gtk-theme
+
+# Icon sets
+sudo apt install faenza-icon-theme -y
