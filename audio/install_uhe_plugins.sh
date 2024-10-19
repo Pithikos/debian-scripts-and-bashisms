c159aa765d48c947644c0b3b97f1f42683dc5c0c
Manos <manossef@gmail.com>
2024-10-19 19:42:02 +0100

    Update UHE installation

--- audio/install_uhe_plugins.sh
+++ audio/install_uhe_plugins.sh
@@ -1,20 +1,25 @@
 #! /bin/bash
-# source: https://www.kvraudio.com/forum/viewtopic.php?f=31&t=424953&hilit=diva+linux+bazille
-# serial number: 
-function install_uhe()
-(
-  product="$1"
-  rev="$2"
-  cd /tmp &&
-  wget http://uhedownloads.heckmannaudiogmb.netdna-cdn.com/penguin/release/$rev/$product-$rev.tar.gz -O $product-$rev.tar.gz &&
-  tar xf $product-$rev.tar.gz &&
-  cd $product-$rev &&
-  ./install.sh --quiet &&
-  cd /tmp &&
-  rm -rf $product-$rev $product-$rev.tar.gz
-)
+function list_linux_packages()
+{
+  curl https://u-he.com/downloads/releases/ | grep 'Linux.tar.xz' | cut -d '<' -f 3 | cut -d '>' -f 2
+}
 
-for product in ACE Bazille Diva Filterscape Hive MFM2 Podolski Presswerk Protoverb "Repro 4365" Satin TrippleCheese Uhbik "Zebra2 4458"
-do
-  install_uhe $product 4408
+# Download all u-he plugins
+dir=$(mktemp -d)
+cd $dir
+for package in $(list_linux_packages); do
+  echo "Downloading $package to $dir"
+  wget https://u-he.com/downloads/releases/$package
+done
+
+# Install all u-he plugins
+for package in $(ls); do
+  echo "Installing $package"
+  install_dirname=$(echo $package | cut -d '.' -f 1)
+  mkdir $install_dirname
+  tar -xvf $package -C $install_dirname
+
+  cd $install_dirname/*
+  ./install.sh --quiet
+  cd ../..
 done
